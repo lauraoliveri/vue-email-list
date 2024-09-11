156ca114f50041799ffb4c86c37ca9cb83dbabf4
lauraoliveri <lauraoliveri2003@gmail.com>
2024-09-11 18:16:01 +0200

    Add random emails

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,22 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            Emails: []
+        }
+    },
+    created() {
+
+        // genero le 10 email random quando viene creato vue
+        for (let i = 0; i < 10; i++) {
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+
+            .then((response)=> {
+                this.email = response.data.response;
+                this.Emails.push(response.data.response);
+            });
         }
     }
     
 }).mount('#app')
+
